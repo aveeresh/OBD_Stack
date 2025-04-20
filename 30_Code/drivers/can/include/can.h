507c5be76603a279505c69b6dc2faa9c411a13f1
LaxmisureshGangal36 <laxmisureshgangal36@gmail.com>
2025-04-20 23:17:39 +0530

    added can_driver with interrupts code

--- 30_Code/drivers/can/include/can.h
+++ 30_Code/drivers/can/include/can.h
@@ -1,6 +1,7 @@
 #ifndef CAN_DRIVER_H
 #define CAN_DRIVER_H
 #include "Types.h"
+#define MAX_CALLBACKS 10
 
 typedef struct {
     uint32 id;
@@ -14,10 +15,21 @@ typedef enum{
 }can_idx_t;
 
 
+typedef void (*can_callback_t)(can_msg_t* msg);
+
+typedef struct {
+    uint32 id;
+    can_callback_t callback;
+} can_callback_entry_t;
+
 void can_init(can_idx_t CanIdx); 
 void can_set_baudrate(can_idx_t CanIdx, uint32 baudrate_Kbps, uint32 PCLK_MHz, uint8 SJW, uint8 TSEG1, uint8 TSEG2);
 void can_transmit(can_idx_t CanIdx, can_msg_t TxMsg);
-uint8 can_receive(can_idx_t CanIdx, can_msg_t *RxMsg); 
+uint8 can_receive(can_idx_t CanIdx, can_msg_t TxMsg); 
+void can_register_callback(uint32 id, can_callback_t callback);
+
+// These handlers should be linked in your startup code or vector table
+void CAN_IRQHandler(void);
 
 
 #endif
--- 30_Code/drivers/can/src/can.c
+++ 30_Code/drivers/can/src/can.c
@@ -8,6 +8,9 @@
 
 static Can_GeneralRegs_t *CanRegs[] = {(Can_GeneralRegs_t*)0x40044000, (Can_GeneralRegs_t*)0x40048000};
 
+static can_callback_entry_t registered_callbacks[MAX_CALLBACKS];
+static int i;
+
 void can_init(can_idx_t CanIdx) 
 {
 	if(CanIdx==can_ctrl_1)
@@ -16,34 +19,22 @@ void can_init(can_idx_t CanIdx)
 	{
 		PCONP.B.PCCAN2 = 0x01;  // Power ON CAN2
 	}
+	CanRegs[CanIdx]->IER.B.RIE = 1; // Enable receive interrupt
+	ISER0_t.B.ISE_CAN=1;   // Enable CAN interrupt in NVIC
+	
 }
 
 void can_set_baudrate(can_idx_t CanIdx, uint32 baudrate_Kbps, uint32 PCLK_MHz, uint8 SJW, uint8 TSEG1, uint8 TSEG2)
 {
-	uint8 BRP;
-	switch (baudrate_Kbps) {
-        case 125:  
-            BRP = (MEG(PCLK_MHz)/ ((KIL(baudrate_Kbps)) *(TSEG1+TSEG2+1)))-1;
-            break;
-        case 250:  
-            BRP = (MEG(PCLK_MHz)/ ((KIL(baudrate_Kbps)) *(TSEG1+TSEG2+1)))-1;
-            break;
-        case 500:  
-            BRP = (MEG(PCLK_MHz)/ ((KIL(baudrate_Kbps)) *(TSEG1+TSEG2+1)))-1; 
-            break;
-        case 1000: 
-            BRP = (MEG(PCLK_MHz)/ ((KIL(baudrate_Kbps)) *(TSEG1+TSEG2+1)))-1;
-            break;
-        
-    }
-    //BRP = (MEG(PCLK_MHz)/ ((KIL(baudrate_Kbps)) *(TSEG1+TSEG2+1)))-1;
+    uint8 BRP = (MEG(PCLK_MHz) / (KIL(baudrate_Kbps) * (TSEG1 + TSEG2 + 1))) - 1;
 		CanRegs[CanIdx]->MOD.B.RM = 1;
 	  CanRegs[CanIdx]->BTR.B.BRP = BRP;
 		CanRegs[CanIdx]->BTR.B.SJW = SJW;
 		CanRegs[CanIdx]->BTR.B.TSEG1 = TSEG1-1;
 		CanRegs[CanIdx]->BTR.B.TSEG2 = TSEG2-1;
-		CanRegs[CanIdx]->MOD.B.RM = 0;
-		//#endif
+	
+    CanRegs[CanIdx]->MOD.B.RM = 0; // Leave reset mode
+	
 }
 
 void can_transmit(can_idx_t CanIdx, can_msg_t msg) 
@@ -53,26 +44,66 @@ void can_transmit(can_idx_t CanIdx, can_msg_t msg)
 		CanRegs[CanIdx]->TID1.StdId.Id = msg.id;   //TxMsg.id.StdId.value;   can_id.StdId.Id
 		CanRegs[CanIdx]->TDA1.R        = *((uint32 *)& msg.data[0]); // Load first 4 bytes
 		CanRegs[CanIdx]->TDB1.R        = *((uint32 *)& msg.data[4]); // Load next 4 bytes
-	  CanRegs[CanIdx]->CMR.R         = (1 << 5) | (1 << 0); // Request transmission
-	  //CanRegs[CanIdx]->CMR.B.TR   = 1;  // Transmission Request
-   // CanRegs[CanIdx]->CMR.B.STB1 = 1;  // Select Tx Buffer 1
-	  
+		CanRegs[CanIdx]->CMR.R         = (1 << 5) | (1 << 0); // Request transmission
 		//while(!((CAN1->GSR)&(0x01<<3)));
 }
 
-uint8 can_receive(can_idx_t CanIdx, can_msg_t *msg)
+uint8 CAN_Receive(uint8 CanIdx, can_msg_t *msg)
 {
-	 // Check if Receive Buffer has a message
-	 if (CanRegs[CanIdx]->SR.B.RBS1) 
-		 {
+	if (CanRegs[CanIdx]->SR.B.RBS1) {
+		// Check if Receive Buffer has a message
             msg->id = CanRegs[CanIdx]->RID.StdId.Id;   // Read Identifier
             msg->len = CanRegs[CanIdx]->RFS.B.DLC; // Extract Data Length
             *((uint32 *)&msg->data[0]) = CanRegs[CanIdx]->RDA.R; // Read first 4 bytes
             *((uint32 *)&msg->data[4]) = CanRegs[CanIdx]->RDB.R; // Read next 4 bytes
             return 1; // Message received successfully
-     }
+        }
 			return 0;  // No message received
 }
+
+// Register callback for a specific CAN ID
+void can_register_callback(uint32 id, can_callback_t callback)
+{
+    for (i = 0; i < MAX_CALLBACKS; ++i) {
+        if (registered_callbacks[i].callback == 0) {
+            registered_callbacks[i].id = id;
+            registered_callbacks[i].callback = callback;
+            break;
+        }
+    }
+}
+
+void CAN_IRQHandler(void)
+{
+     can_msg_t *msg;
+     can_idx_t CanIdx;
+	
+	// Check if a message is available in RX buffer
+    if (CanRegs[CanIdx]->SR.B.RBS1) 
+    {
+        // Read CAN message
+	   msg->id = CanRegs[CanIdx]->RID.StdId.Id;   // Read Identifier
+     msg->len = CanRegs[CanIdx]->RFS.B.DLC; // Extract Data Length
+     *((uint32 *)&msg->data[0]) = CanRegs[CanIdx]->RDA.R; // Read first 4 bytes
+     *((uint32 *)&msg->data[4]) = CanRegs[CanIdx]->RDB.R; // Read next 4 bytes
+	   CanRegs[CanIdx]->CMR.B.RRB=1; // release RX buffer
+
+      for (i = 0; i < MAX_CALLBACKS; ++i) 
+	    {
+        //if (registered_callbacks[i].callback != 0 && registered_callbacks[i].id == msg->id) 
+				// Match registered ID exactly
+            // Or match functional ID 0x7DF
+            // Or if 0x7E0 is registered, match range 0x7E0–0x7EF
+				 if (msg->id == registered_callbacks[i].id ||
+                (registered_callbacks[i].id == 0x7DF && msg->id == 0x7DF) ||
+                (registered_callbacks[i].id == 0x7E0 && msg->id >= 0x7E0 && msg->id <= 0x7EF))
+				{
+            registered_callbacks[i].callback(msg);
+        }
+      }
+    }		
+}
+
 
 
 
