a84f5fca02764169a185df01f2dd4f4c40c053ff
Shwetamasali <105476336+Shwetamasali@users.noreply.github.com>
2025-04-22 18:05:51 +0530

    Updated mode_09.c

--- 30_Code/services/Mode_09/src/mode_09.c
+++ 30_Code/services/Mode_09/src/mode_09.c
@@ -1,9 +1,8 @@
-#include "Mode_09.h"
-#include "Types.h"
- 
-void Mode_09_Req_Handler(can_msg_t OBDRequest)
+#include "mode_09.h"
+
+void mode_09_req_handler(can_msg_t OBDRequest)
 {
-	int i;
+    int i;
     uint8 PID = OBDRequest.data[2];  // Get the PID from the incoming message
     can_msg_t OBDResp;  // Declare a CAN response message
 
@@ -22,7 +21,7 @@ void Mode_09_Req_Handler(can_msg_t OBDRequest)
             OBDResp.data[4] = 0x00;
             OBDResp.data[5] = 0x04;
             OBDResp.data[6] = 0x00;
-        can_tp_transmit(can_idx, OBDResp, OBDResp.len);
+        can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
             break;
         }
         case 0x02: // VIN (17 characters)
@@ -39,19 +38,19 @@ void Mode_09_Req_Handler(can_msg_t OBDRequest)
             OBDResp.data[5] = VIN[1];
             OBDResp.data[6] = VIN[2];
             OBDResp.data[7] = VIN[3];
-     can_tp_transmit(can_idx, OBDResp, OBDResp.len);
+     can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
 
             // Second frame
             OBDResp.data[0] = 0x21;
             for (i = 0; i < 7; i++)
                 OBDResp.data[i + 1] = VIN[i + 4];
-   can_tp_transmit(can_idx, OBDResp, OBDResp.len);
+   can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
 
             // Third frame
             OBDResp.data[0] = 0x22;
             for (i = 0; i < 7; i++)
                 OBDResp.data[i + 1] = VIN[i + 11];
-    can_tp_transmit(can_idx, OBDResp, OBDResp.len);
+    can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
             break;
         }
 
@@ -69,19 +68,19 @@ void Mode_09_Req_Handler(can_msg_t OBDRequest)
             OBDResp.data[5] = CALID[1];
             OBDResp.data[6] = CALID[2];
             OBDResp.data[7] = CALID[3];
-         can_tp_transmit(can_idx, OBDResp, OBDResp.len);
+         can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
 
             // Second frame
             OBDResp.data[0] = 0x21;
             for (i = 0; i < 7; i++)
                 OBDResp.data[i + 1] = CALID[i + 4];
-        can_tp_transmit(can_idx, OBDResp, OBDResp.len);
+        can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
 
             // Third frame
             OBDResp.data[0] = 0x22;
             for (i = 0; i < 8; i++)
                 OBDResp.data[i + 1] = CALID[i + 11];
-           can_tp_transmit(can_idx, OBDResp, OBDResp.len);
+           can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
             break;
         }
 
@@ -99,17 +98,17 @@ void Mode_09_Req_Handler(can_msg_t OBDRequest)
             OBDResp.data[5] = ECUName[1];
             OBDResp.data[6] = ECUName[2];
             OBDResp.data[7] = ECUName[3];
-           can_tp_transmit(can_idx, OBDResp, OBDResp.len);
+           can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
 
             OBDResp.data[0] = 0x21;
             for (i = 0; i < 7; i++)
                 OBDResp.data[i + 1] = ECUName[i + 4];
-          can_tp_transmit(can_idx, OBDResp, OBDResp.len);
+          can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
 
             OBDResp.data[0] = 0x22;
             for ( i = 0; i < 8; i++)
                 OBDResp.data[i + 1] = (i + 11 < nameLen) ? ECUName[i + 11] : 0x00;
-          can_tp_transmit(can_idx, OBDResp, OBDResp.len);
+          can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
             break;
         }
 
@@ -121,7 +120,7 @@ void Mode_09_Req_Handler(can_msg_t OBDRequest)
             OBDResp.data[1] = 0x7F;
             OBDResp.data[2] = 0x09;
             OBDResp.data[3] = 0x12;
-           can_tp_transmit(can_idx, OBDResp, OBDResp.len);
+           can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
             break;
         }
     }
