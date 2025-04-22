ac30bce33b3dc4cfdeecffdd9fdb635321a0ccb1
Shwetamasali <105476336+Shwetamasali@users.noreply.github.com>
2025-04-22 18:10:23 +0530

    Updated mode_09.h

--- 30_Code/services/Mode_09/include/mode_09.h
+++ 30_Code/services/Mode_09/include/mode_09.h
@@ -1,14 +1,10 @@
 #ifndef MODE_09_H
 #define MODE_09_H
 
-// CAN Message structure
-typedef struct {
-    uint32 id;       // CAN ID
-    uint8 len;       // Length of the data
-    uint8 data[8];   // CAN message data (8 bytes)
-} CanMsg_t;
+#include "Can_Driver.h"
+#include "cantp.h"
 
 // Function prototypes
-void Mode_09_Req_Handler(CanMsg_t OBDRequest);
+void mode_09_req_handler(can_msg_t OBDRequest);
 
 #endif // MODE_09_HANDLER_H
