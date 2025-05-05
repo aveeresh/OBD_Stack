c8ee6f397efa2f570449422f17865a2261a51fb1
Shwetamasali <105476336+Shwetamasali@users.noreply.github.com>
2025-05-05 11:49:11 +0530

    Updated mode_09.c

--- 30_Code/services/Mode_09/src/mode_09.c
+++ 30_Code/services/Mode_09/src/mode_09.c
@@ -1,114 +1,60 @@
 #include "mode_09.h"
+#include <string.h>
 
 void mode_09_req_handler(can_msg_t OBDRequest)
 {
     int i;
     uint8 PID = OBDRequest.data[2];  // Get the PID from the incoming message
     can_msg_t OBDResp;  // Declare a CAN response message
+    uint8 response_data[64];  // Buffer for response data
+    uint8 response_len = 0;   // Length of data in the response buffer
 
     OBDResp.id = 0x7E8;  // Set response CAN ID
 
     switch(PID)
     {
-        case 0x00: // Supported PIDs 
+        case 0x00: // Supported PIDs
         {
-            // Set the supported PIDs as a bitmask
-            OBDResp.len = 8;
-            OBDResp.data[0] = 0x06;
-            OBDResp.data[1] = 0x49;
-            OBDResp.data[2] = 0x00;
-            OBDResp.data[3] = 0x50;
-            OBDResp.data[4] = 0x00;
-            OBDResp.data[5] = 0x04;
-            OBDResp.data[6] = 0x00;
-        can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
-            break;
-        }
-        case 0x02: // VIN (17 characters)
-        {
-            char VIN[17] = "WAUZAB345XY123456";  // VIN number
-
-            // First frame
-            OBDResp.len = 8;
-            OBDResp.data[0] = 0x10;
-            OBDResp.data[1] = 0x14;
-            OBDResp.data[2] = 0x49;
-            OBDResp.data[3] = 0x02;
-            OBDResp.data[4] = VIN[0];
-            OBDResp.data[5] = VIN[1];
-            OBDResp.data[6] = VIN[2];
-            OBDResp.data[7] = VIN[3];
-     can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
-
-            // Second frame
-            OBDResp.data[0] = 0x21;
-            for (i = 0; i < 7; i++)
-                OBDResp.data[i + 1] = VIN[i + 4];
-   can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
-
-            // Third frame
-            OBDResp.data[0] = 0x22;
-            for (i = 0; i < 7; i++)
-                OBDResp.data[i + 1] = VIN[i + 11];
-    can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
+            response_data[response_len++] = 0x49;
+            response_data[response_len++] = 0x00;
+            response_data[response_len++] = 0x50;
+            response_data[response_len++] = 0x04;
+            response_data[response_len++] = 0x00;
+            response_data[response_len++] = 0x00;
             break;
         }
 
-        case 0x04: // Calibration ID (16 characters)
+        case 0x02: // VIN (configured size 17 characters)
         {
-            char CALID[16] = "ECU_CAL_ID_12345";
+            char VIN[PID_0x02_SIZE] = "WAUZAB345XY123456";  // VIN number
 
-            // First frame
-            OBDResp.len = 8;
-            OBDResp.data[0] = 0x10;
-            OBDResp.data[1] = 0x13;
-            OBDResp.data[2] = 0x49;
-            OBDResp.data[3] = 0x04;
-            OBDResp.data[4] = CALID[0];
-            OBDResp.data[5] = CALID[1];
-            OBDResp.data[6] = CALID[2];
-            OBDResp.data[7] = CALID[3];
-         can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
-
-            // Second frame
-            OBDResp.data[0] = 0x21;
-            for (i = 0; i < 7; i++)
-                OBDResp.data[i + 1] = CALID[i + 4];
-        can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
-
-            // Third frame
-            OBDResp.data[0] = 0x22;
-            for (i = 0; i < 8; i++)
-                OBDResp.data[i + 1] = CALID[i + 11];
-           can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
+            response_data[response_len++] = 0x49;
+            response_data[response_len++] = 0x02;
+            for (i = 0; i < PID_0x02_SIZE; i++)
+                response_data[response_len++] = VIN[i];
             break;
         }
 
-        case 0x0A: // ECU Name (ASCII string, padded to 20)
+        case 0x04: // Calibration ID (configured size 16 characters)
         {
-            char ECUName[20] = "Engine_Control_ECU";
+            char CALID[PID_0x04_SIZE] = "ECU_CAL_ID_12345";
+
+            response_data[response_len++] = 0x49;
+            response_data[response_len++] = 0x04;
+            for (i = 0; i < PID_0x04_SIZE; i++)
+                response_data[response_len++] = CALID[i];
+            break;
+        }
+
+        case 0x0A: // ECU Name (configured size 20 characters)
+        {
+            char ECUName[PID_0x0A_SIZE] = "Engine_Control_ECU";
             uint8 nameLen = strlen(ECUName);
 
-            OBDResp.len = 8;
-            OBDResp.data[0] = 0x10;
-            OBDResp.data[1] = 0x17;
-            OBDResp.data[2] = 0x49;
-            OBDResp.data[3] = 0x0A;
-            OBDResp.data[4] = ECUName[0];
-            OBDResp.data[5] = ECUName[1];
-            OBDResp.data[6] = ECUName[2];
-            OBDResp.data[7] = ECUName[3];
-           can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
-
-            OBDResp.data[0] = 0x21;
-            for (i = 0; i < 7; i++)
-                OBDResp.data[i + 1] = ECUName[i + 4];
-          can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
-
-            OBDResp.data[0] = 0x22;
-            for ( i = 0; i < 8; i++)
-                OBDResp.data[i + 1] = (i + 11 < nameLen) ? ECUName[i + 11] : 0x00;
-          can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
+            response_data[response_len++] = 0x49;
+            response_data[response_len++] = 0x0A;
+            for (i = 0; i < PID_0x0A_SIZE; i++)
+                response_data[response_len++] = (i < nameLen) ? ECUName[i] : 0x00;
             break;
         }
 
@@ -120,8 +66,15 @@ void mode_09_req_handler(can_msg_t OBDRequest)
             OBDResp.data[1] = 0x7F;
             OBDResp.data[2] = 0x09;
             OBDResp.data[3] = 0x12;
-           can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
-            break;
+            can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);
+            return;
         }
     }
+
+    // Prepare final response frame
+    for (i = 0; i < response_len; i++)
+        OBDResp.data[i] = response_data[i];
+    OBDResp.len = response_len;
+
+    can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);  // Send the response
 }
