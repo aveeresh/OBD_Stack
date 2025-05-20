bf5c4df8cb3cc12d8a671b09b363bb954bfd52ea
LaxmisureshGangal36 <105476492+LaxmisureshGangal36@users.noreply.github.com>
2025-05-20 13:25:01 +0530

    Updated mode_01.c

--- 30_Code/services/Mode_01/src/mode_01.c
+++ 30_Code/services/Mode_01/src/mode_01.c
@@ -1,7 +1,9 @@
-void Mode_01_Req_Handler(CanMsg_t OBDRequest) {
+#include "mode_01.h"
+void mode_01_req_handler(can_msg_t OBDRequest) {
     uint8 A = 0, B = 0, C = 0, D = 0;
-    CAN_msg OBDResp;
+   can_msg_t OBDResp;
     uint8 i;
+	uint8 len;
 	uint8 PID = OBDRequest.data[2]; 
 
     // List of supported Mode 01 PIDs
@@ -10,8 +12,8 @@ void Mode_01_Req_Handler(CanMsg_t OBDRequest) {
         0x2D, 0x2F, 0x40,
         0x45, 0x46, 0x60,
         0x62, 0x63, 0x80,
-        0x85, 0x8E, 0xA0, 0xA2,
-        0xC0, 0xC3, 0xC4
+        0x85, 0x8E, 0xA0, 0xA4,0xA5,
+        0x0C,0x22,0x2C,0x31
     };
 
     uint8 supported = 0;
@@ -29,8 +31,8 @@ void Mode_01_Req_Handler(CanMsg_t OBDRequest) {
     if (!supported) return; // PID not supported
 
     OBDResp.id = 0x7E8;
-    OBDResp.data[1] = 0x41; // Response to Mode 01
-    OBDResp.data[2] = PID;
+    OBDResp.data[0] = 0x41; // Response to Mode 01
+    OBDResp.data[1] = PID;
 
     switch (PID) {
         case 0x00:  // Supported PIDs [01 - 20]
@@ -38,47 +40,74 @@ void Mode_01_Req_Handler(CanMsg_t OBDRequest) {
             B = (PIDValues[PID_IDX_SUPPORTED_00] >> 16) & 0xFF;
             C = (PIDValues[PID_IDX_SUPPORTED_00] >> 8) & 0xFF;
             D = PIDValues[PID_IDX_SUPPORTED_00] & 0xFF;
-            OBDResp.data[3] = A;
-            OBDResp.data[4] = B;
-            OBDResp.data[5] = C;
-            OBDResp.data[6] = D;
+            OBDResp.data[2] = A;
+            OBDResp.data[3] = B;
+            OBDResp.data[4] = C;
+            OBDResp.data[5] = D;
             len = 6;
             break;
 
         case 0x04:  // Calculated Engine Load (%)
             A = (PIDValues[PID_IDX_ENGINE_LOAD_04] * 255) / 100;
-            OBDResp.data[3] = A;
+            OBDResp.data[2] = A;
             len = 3;
             break;
 
-        case 0x05:  // Engine Coolant Temperature (°C + 40)
+        case 0x05:  // Engine Coolant Temperature 
             A = PIDValues[PID_IDX_COOLANT_TEMP_05] + 40;
-            OBDResp.data[3] = A;
+            OBDResp.data[2] = A;
             len = 3;
             break;
-
+				
+       case 0x0C:  // Engine RPM
+            // Formula: ((A * 256) + B) / 4
+            //uint16 rpm = PIDValues[PID_IDX_ENGINE_RPM_0C];  // rpm is a uint16_t
+            A = (PIDValues[PID_IDX_ENGINE_RPM_0C] >> 8) & 0xFF;
+            B = PIDValues[PID_IDX_ENGINE_RPM_0C] & 0xFF;
+            OBDResp.data[2] = A;
+            OBDResp.data[3] = B;
+            len = 4;
+            break;
+				
         case 0x20:  // Supported PIDs [21 - 40]
             A = (PIDValues[PID_IDX_SUPPORTED_20] >> 24) & 0xFF;
             B = (PIDValues[PID_IDX_SUPPORTED_20] >> 16) & 0xFF;
             C = (PIDValues[PID_IDX_SUPPORTED_20] >> 8) & 0xFF;
             D = PIDValues[PID_IDX_SUPPORTED_20] & 0xFF;
-            OBDResp.data[3] = A;
-            OBDResp.data[4] = B;
-            OBDResp.data[5] = C;
-            OBDResp.data[6] = D;
+            OBDResp.data[2] = A;
+            OBDResp.data[3] = B;
+            OBDResp.data[4] = C;
+            OBDResp.data[5] = D;
             len = 6;
             break;
+				
+        case 0x2C:  // Commanded EGR (%)
+            // Formula: A * 100 / 255
+            A = (PIDValues[PID_IDX_COMMANDED_EGR_2C] * 255) / 100;
+            OBDResp.data[2] = A;
+            len = 3;
+            break;
 
-        case 0x2D:  // Fuel Rail Pressure
-            A = ((PIDValues[PID_IDX_FUEL_RAIL_PRESSURE_2D] + 100) * 128) / 100;
-            OBDResp.data[3] = A;
+         case 0x2D:  // EGR Error
+            A = ((PIDValues[PID_IDX_EGR_Error_2D] + 100) * 128) / 100;
+            OBDResp.data[2] = A;
             len = 3;
             break;
 
         case 0x2F:  // Fuel Level Input (%)
             A = (PIDValues[PID_IDX_FUEL_LEVEL_2F] * 255) / 100;
-            OBDResp.data[3] = A;
+            OBDResp.data[2] = A;
             len = 3;
+            break;
+        
+	case 0x31:  // Distance since DTCs cleared (km)
+            // Formula: (A * 256) + B
+            //distance = PIDValues[PID_IDX_DISTANCE_SINCE_DTC_31];  // in km
+            A = (PIDValues[PID_IDX_DISTANCE_SINCE_DTC_31] >> 8) & 0xFF;
+            B = PIDValues[PID_IDX_DISTANCE_SINCE_DTC_31] & 0xFF;
+            OBDResp.data[2] = A;
+            OBDResp.data[3] = B;
+            len = 4;
             break;
 
         case 0x40:  // Supported PIDs [41 - 60]
@@ -86,22 +115,22 @@ void Mode_01_Req_Handler(CanMsg_t OBDRequest) {
             B = (PIDValues[PID_IDX_SUPPORTED_40] >> 16) & 0xFF;
             C = (PIDValues[PID_IDX_SUPPORTED_40] >> 8) & 0xFF;
             D = PIDValues[PID_IDX_SUPPORTED_40] & 0xFF;
-            OBDResp.data[3] = A;
-            OBDResp.data[4] = B;
-            OBDResp.data[5] = C;
-            OBDResp.data[6] = D;
+            OBDResp.data[2] = A;
+            OBDResp.data[3] = B;
+            OBDResp.data[4] = C;
+            OBDResp.data[5] = D;
             len = 6;
             break;
 
-        case 0x45:  // Ambient Air Temperature (%)
+        case 0x45:  // Relative throttle position (%)
             A = (PIDValues[PID_IDX_AMBIENT_TEMP_45] * 255) / 100;
-            OBDResp.data[3] = A;
+            OBDResp.data[2] = A;
             len = 3;
             break;
 
-        case 0x46:  // Absolute Barometric Pressure (+40)
+        case 0x46:  //Ambient air temperature  (+40)
             A = PIDValues[PID_IDX_ABS_BARO_PRESSURE_46] + 40;
-            OBDResp.data[3] = A;
+            OBDResp.data[2] = A;
             len = 3;
             break;
 
@@ -110,24 +139,24 @@ void Mode_01_Req_Handler(CanMsg_t OBDRequest) {
             B = (PIDValues[PID_IDX_SUPPORTED_60] >> 16) & 0xFF;
             C = (PIDValues[PID_IDX_SUPPORTED_60] >> 8) & 0xFF;
             D = PIDValues[PID_IDX_SUPPORTED_60] & 0xFF;
-            OBDResp.data[3] = A;
-            OBDResp.data[4] = B;
-            OBDResp.data[5] = C;
-            OBDResp.data[6] = D;
+            OBDResp.data[2] = A;
+            OBDResp.data[3] = B;
+            OBDResp.data[4] = C;
+            OBDResp.data[5] = D;
             len = 6;
             break;
 
-        case 0x62:  // Commanded Equivalence Ratio (+125)
-            A = PIDValues[PID_IDX_COMMANDED_EQUIV_RATIO_62] + 125;
-            OBDResp.data[3] = A;
+        case 0x62:  // Actual engine - percent torque (+125)
+            A = PIDValues[PID_IDX_Actual_engine_percent_torque_62] + 125;
+            OBDResp.data[2] = A;
             len = 3;
             break;
 
         case 0x63:  // Engine Reference Torque (2 bytes)
             A = (PIDValues[PID_IDX_ENGINE_REFERENCE_TORQUE_63] >> 8) & 0xFF;
             B = PIDValues[PID_IDX_ENGINE_REFERENCE_TORQUE_63] & 0xFF;
-            OBDResp.data[3] = A;
-            OBDResp.data[4] = B;
+            OBDResp.data[2] = A;
+            OBDResp.data[3] = B;
             len = 4;
             break;
 
@@ -136,48 +165,79 @@ void Mode_01_Req_Handler(CanMsg_t OBDRequest) {
             B = (PIDValues[PID_IDX_SUPPORTED_80] >> 16) & 0xFF;
             C = (PIDValues[PID_IDX_SUPPORTED_80] >> 8) & 0xFF;
             D = PIDValues[PID_IDX_SUPPORTED_80] & 0xFF;
-            OBDResp.data[3] = A;
-            OBDResp.data[4] = B;
-            OBDResp.data[5] = C;
-            OBDResp.data[6] = D;
+            OBDResp.data[2] = A;
+            OBDResp.data[3] = B;
+            OBDResp.data[4] = C;
+            OBDResp.data[5] = D;
             len = 6;
             break;
 
-        case 0x85:  // Throttle Position (%)
-            A = (PIDValues[PID_IDX_THROTTLE_POS_85] * 255) / 100;
-            OBDResp.data[3] = A;
+        case 0x85:  // NOx reagent system
+            A = (PIDValues[PID_IDX_NOx_reagent_system_85] * 255) / 100;
+            OBDResp.data[2] = A;
             len = 3;
             break;
 
-        case 0x8E:  // Engine Friction Percent Torque (+125)*2
-            A = (uint8)((PIDValues[PID_IDX_ENGINE_FRICTION_PERCENT_8E] + 125) * 2);
-            OBDResp.data[3] = A;
+        case 0x8E:  // Engine Friction Percent Torque 
+            A = (PIDValues[PID_IDX_ENGINE_FRICTION_PERCENT_8E] + 125) ;
+            OBDResp.data[2] = A;
             len = 3;
             break;
 
-        case 0xA0:  // Supported PIDs [A1 - C0]
+        case 0xA0: 
             A = (PIDValues[PID_IDX_SUPPORTED_A0] >> 24) & 0xFF;
             B = (PIDValues[PID_IDX_SUPPORTED_A0] >> 16) & 0xFF;
             C = (PIDValues[PID_IDX_SUPPORTED_A0] >> 8) & 0xFF;
             D = PIDValues[PID_IDX_SUPPORTED_A0] & 0xFF;
-            OBDResp.data[3] = A;
-            OBDResp.data[4] = B;
-            OBDResp.data[5] = C;
-            OBDResp.data[6] = D;
+            OBDResp.data[2] = A;
+            OBDResp.data[3] = B;
+            OBDResp.data[4] = C;
+            OBDResp.data[5] = D;
             len = 6;
             break;
+				
+				
+	case 0xA4:  
+       {
+             float value = PIDValues[PID_IDX_Transmission_Actual_Gear_A4];  
+             uint16 raw = (uint16)(value * 1000);  
 
-        case 0xA2:  // Engine Percent Torque (scaled)
-            A = (PIDValues[PID_IDX_ENGINE_PERCENT_TORQUE_A2] * 128) / 100;
-            OBDResp.data[3] = A;
-            len = 3;
+             A = (raw >> 8) & 0xFF;
+             B = raw & 0xFF;
+
+            OBDResp.data[2] = A;
+            OBDResp.data[3] = B;
+            len = 4;
             break;
+      }
+
+
+       case 0xA5:  
+      {
+             float value = PIDValues[PID_IDX_Commanded_Diesel_Exhaust_Fluid_Dosing_A5]; 
+             B= (uint8 )(value * 2); 
+
+             OBDResp.data[2] = B;
+             len = 3;
+             break;
+       }
+ 
 
         default:
             return; // Do nothing if PID is not matched
     }
+		if(len<6)
+		{
+			for (i = len; i < 7; i++)
+			{  // padding up to index 6
+                            OBDResp.data[i] = 0;
+                        }
+                       OBDResp.data[7] = 0;         // Ensure full 8-byte frame
+		}
+		
 
-    OBDResp.data[0] = len;  // Set response length
-    OBDResp.len = len + 1;
-    can_tp_transmit(CANCTRL1, OBDResp, OBDResp.len);  // Transmit the response
+
+    //OBDResp.data[0] = len;  // Set response length
+    OBDResp.len = len;
+    can_tp_transmit(can_ctrl_1, OBDResp, OBDResp.len);  // Transmit the response
 }
